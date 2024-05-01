1946282ebc430712f8390019fa4df0ca8607043f
Eric Haag <ehaag@gradle.com>
2024-05-01 14:07:27 +0200

    Make init script first class citizen

--- demo-project/run.sh
+++ demo-project/run.sh
@@ -4,7 +4,7 @@ cd .. && ./gradlew clean publishAllPublicationsToLocalRepository
 
 cd demo-project
 
-./gradlew -I demo.init.gradle \
+./gradlew -I ../reference/configure-develocity.gradle \
     -Ddevelocity.url=https://ge.solutions-team.gradle.com/ \
     -Ddevelocity.injection-enabled=true \
     -Ddevelocity.plugin.version=3.17.2 \
