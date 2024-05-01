48c3e6083cb5c19f3d114f77022ff3ae9ec16ccf
daz <daz@gradle.com>
2024-05-01 07:53:47 +0200

    Remove generated tests

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     `java-gradle-plugin`
     groovy
@@ -9,35 +8,9 @@ repositories {
     mavenCentral()
 }
 
-testing {
-    suites {
-        val test by getting(JvmTestSuite::class) {
-            useJUnitJupiter("5.10.2")
-        }
-
-        val functionalTest by registering(JvmTestSuite::class) {
-            dependencies {
-                implementation(project())
-            }
-
-            targets {
-                all {
-                    testTask.configure { shouldRunAfter(test) } 
-                }
-            }
-        }
-    }
-}
-
 gradlePlugin {
     val greeting by plugins.creating {
         id = "com.gradle.develocity-injection"
         implementationClass = "com.gradle.DevelocityInjectionGradlePlugin"
     }
-}
-
-gradlePlugin.testSourceSets.add(sourceSets["functionalTest"])
-
-tasks.named<Task>("check") {
-    dependsOn(testing.suites.named("functionalTest"))
 }
